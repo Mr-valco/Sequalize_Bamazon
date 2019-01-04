0ea3f69695a7dace08b03908852c79de02ce9121
Valentin Meica <valentinmeica@gmail.com>
2019-01-03 19:00:37 -0500

    config and models

---
+++ models/product.js
@@ -0,0 +1,36 @@
+//Define table
+
+module.exports = function (connection, Sequelize) {
+    var Product = connection.define('Product', {
+        product_name: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        },
+        department_name: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        },
+        price: {
+            type: Sequelize.REAL,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        },
+        stock_quantity: {
+            type: Sequelize.INTEGER,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        }
+    });
+    
+    return Product;
+};
